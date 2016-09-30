0fea6bc0a3f23f4ef4f11d3c4c56546015853af8
Wang Cong <ousatoru@hotmail.com>
2016-09-30 23:19:18 +0800

    can't print sha512

---
+++ ch4/ex4_2/main.go
@@ -0,0 +1,29 @@
+//print SHA256 default, and SHA384 SHA512, did not use flag
+
+package main
+
+import (
+	"os"
+	"fmt"
+	"crypto/sha256"
+	"crypto/sha512"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("Nothing typed.")
+	} else if len(os.Args) == 2 {
+		s1 := sha256.Sum256([]byte(os.Args[1]))
+		fmt.Printf("SHA256 of %s: %x", os.Args[1], s1)
+	} else if os.Args[1] == "sha384" {
+		s2 := sha512.Sum384([]byte(os.Args[2]))
+		fmt.Printf("SHA384 of %s: %x", os.Args[2], s2)
+	} else if os.Args[1] == "sha512" {  //can't print sha512
+		s3 := sha512.Sum512([]byte(os.Args[2]))
+		fmt.Printf("SHA512 of %s: ", os.Args[3])
+		fmt.Println(s3)
+	} else if os.Args[1] == "sha256" {
+		s1 := sha256.Sum256([]byte(os.Args[1]))
+		fmt.Printf("SHA256 of %s: %x", os.Args[1], s1)
+	}
+}
